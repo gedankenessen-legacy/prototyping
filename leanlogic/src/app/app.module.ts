58ae04621fe208a06e53cbb60a8572104e6f6735
marlonschlosshauer <marlonschlosshauer@gmail.com>
2020-12-26 18:32:25 +0100

    Fix imports

--- leanlogic/src/app/app.module.ts
+++ leanlogic/src/app/app.module.ts
@@ -2,27 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
+import { AddModalComponent } from './add-modal/add-modal.component';
+
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzFormModule } from 'ng-zorro-antd/form';
 
 registerLocaleData(en);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule
-  ],
-  providers: [{ provide: NZ_I18N, useValue: en_US }],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		AddModalComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		HttpClientModule,
+		BrowserAnimationsModule,
+		NzTableModule,
+		NzButtonModule,
+		NzModalModule,
+		NzDatePickerModule,
+		NzFormModule,
+		ReactiveFormsModule,
+	],
+	providers: [{ provide: NZ_I18N, useValue: en_US }],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
