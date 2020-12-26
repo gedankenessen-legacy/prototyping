e9b272a2b833adc9784b2ed4c1380c59a0e9605a
marlonschlosshauer <marlonschlosshauer@gmail.com>
2020-12-26 18:31:45 +0100

    Fix filter

--- leanlogic/src/app/employee.service.ts
+++ leanlogic/src/app/employee.service.ts
@@ -8,13 +8,23 @@ import { Employee } from './models/Employee';
 export class EmployeeService {
 	// Dummy Daten, aggieren als Datenbank
 	private data = [
-		new Employee({ id: 0, surname: 'Dieter' }),
+		new Employee({ id: 0, surname: 'Dieter', lastname: 'Dieter', birthday: new Date('1965-10-22T12:00') }),
+		new Employee({ id: 1, surname: 'Hans', lastname: 'Hubschrauber', birthday: new Date('1959-09-12T10:00') }),
+		new Employee({ id: 2, surname: 'Gertrud', lastname: 'Gut', birthday: new Date('1990-07-08T22:00') }),
+		new Employee({ id: 3, surname: 'Dagma', lastname: 'Dieter', birthday: new Date('2002-08-13T07:00') }),
+		new Employee({ id: 4, surname: 'Hilde', lastname: 'Haube', birthday: new Date('1982-05-04T14:00') }),
+		new Employee({ id: 5, surname: 'Peter', lastname: 'Panne', birthday: new Date('1970-06-24T15:00') }),
+		new Employee({ id: 6, surname: 'Jasmin', lastname: 'Januar', birthday: new Date('2000-10-11T23:00') }),
+		new Employee({ id: 7, surname: 'Jorg', lastname: 'Josman', birthday: new Date('1999-03-02T21:00') }),
+		new Employee({ id: 8, surname: 'Gerhard', lastname: 'Grube', birthday: new Date('1959-11-23T18:00') }),
+		new Employee({ id: 9, surname: 'Niels', lastname: 'Niemand', birthday: new Date('1975-12-30T09:00') }),
+		new Employee({ id: 11, surname: 'Joseph', lastname: 'Jojo', birthday: new Date('1989-04-15T15:00') }),
 	];
 
 	constructor() { }
 
 	public getEmployees(filter: string): Observable<Employee[]> {
-		return of(this.data.filter(x => `${x.surname} ${x.lastname}`.includes(filter)));
+		return (filter) ? of(this.data.filter(x => `${x.surname} ${x.lastname}`.includes(filter))) : of(this.data);
 	}
 
 	public addEmployee(employee: Employee): Observable<boolean> {
