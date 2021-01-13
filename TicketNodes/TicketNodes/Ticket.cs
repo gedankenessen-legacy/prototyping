9fc8e015c4b1da6e52f00c83020a47ca3dffb1ff
Yochyo <herodragon25@gmail.com>
2021-01-13 17:38:23 +0100

    Added missing cases in CanAddPredecessor

--- TicketNodes/TicketNodes/Ticket.cs
+++ TicketNodes/TicketNodes/Ticket.cs
@@ -96,17 +96,37 @@ namespace TicketNodes
             return this.GetRoot().Equals(other.GetRoot());
         }
 
+        public bool IsChildOf(Ticket other)
+        {
+            return other.GetChildrenRecursive().FirstOrDefault(child => child.Equals(this)) != null;
+        }
+
         private bool CanAddPredecessor(Ticket other)
         {
+            //Falls A Ein Ober-/Unter-ticket von B ist 
+            if (IsChildOf(other) || other.IsChildOf(this)) return false;
+            
+            //Falls A schon einen Vorgänger hat
+            if (Predecessor != null) return false;
+            
             Queue<Ticket> tickets = new Queue<Ticket>();
+            //fügt sich selber und alle Kinder hinzu. Die Kinder werden hinzugefügt, da der Vorgänger eines Obertickets der Vorgänger aller 
+            //Untertickets ist
             tickets.Enqueue(this);
+            foreach (var ticket in GetChildrenRecursive())
+                tickets.Enqueue(ticket);
+            
+            /*
+             * Iteriert durch jedes Ticket bis die Queue leer ist oder element.Equals(other) true ist. Bei jeder Iteration werden Eltern
+             * und alle Nachfolger (auch indirekte) der Queue hinzugefügt
+             */
             while (tickets.Count > 0)
             {
                 var element = tickets.Dequeue();
                 if (element == null) continue;
                 if (element.Equals(other)) return false;
                 if (element.Parent != null) tickets.Enqueue(element.Parent);
-                foreach (var ticket in element.Successors)
+                foreach (var ticket in element.GetSuccessorsAdvanced())
                     tickets.Enqueue(ticket);
             }
 
